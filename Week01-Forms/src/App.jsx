cfdbf3b94ee113825a73dfdb69fc6bb3f336a3d3
jagrutib <jagrutibangar27@gmail.com>
2025-05-23 07:34:10 +0530

    Added Week 1 form components

---
+++ Week01-Forms/src/App.jsx
@@ -0,0 +1,18 @@
+import FormHeader from './components/FormHeader'
+
+function App() {
+
+
+  return (
+  <>
+  {/* {/* Form header  */}
+  <FormHeader />
+  {/* Main Form */}
+
+  {/* Form Footer  */}
+  </>
+
+  )
+}
+
+export default App
---
+++ Week01-Forms/src/components/FormHeader.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const FormHeader = () => {
+    return(
+        <>
+            <div style={{textAlign:"center", fontStyle:"serif", color:"brown", padding:20, borderRadius:10, marginTop:2}}>
+            <h1 className="form-header" style={{fontFamily:"serif", fontSize:30}}>Welcome!</h1>
+            <p className="form-subheader" style={{fontFamily:"serif", fontSize:20}}>Fill in the Form Below</p>
+            <hr className="mb-4"/>
+            </div>
+        </>
+    );
+};
+
+
+export default FormHeader;
